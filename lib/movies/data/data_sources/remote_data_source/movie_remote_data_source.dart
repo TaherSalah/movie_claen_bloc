6a4db9320c2bfa14ef58b63be837b2d35a51d76c
TaherSalah <tahersalah2016@gmail.com>
2023-07-22 19:11:56 +0300

    add abstract class for remote for contract the method

--- lib/movies/data/data_sources/remote_data_source/movie_remote_data_source.dart
+++ lib/movies/data/data_sources/remote_data_source/movie_remote_data_source.dart
@@ -1,10 +1,17 @@
 import 'package:movie_db_bloc/core/exports/exports_files.dart';
 
+abstract class BaseRemoteMovieDataSource {
+  Future<List<MovieModel>> getNowPlayingMovie();
 
+  Future<List<MovieModel>> getPopularMovie();
 
-class MovieRemoteDataSource {
+  Future<List<MovieModel>> getTpoRatedMovie();
+}
+
+class MovieRemoteDataSource extends BaseRemoteMovieDataSource {
   final Dio dio = Dio();
 
+  @override
   Future<List<MovieModel>> getNowPlayingMovie() async {
     final response = await dio.get(AppConstance.baseUrl);
     if (response.statusCode == 200) {
@@ -17,5 +24,17 @@ class MovieRemoteDataSource {
           ServerException(
               errorMessageModel: ErrorMessageModel.fromJson(response.data)));
     }
+  }
+
+  @override
+  Future<List<MovieModel>> getPopularMovie() {
+    // TODO: implement getPopularMovie
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<MovieModel>> getTpoRatedMovie() {
+    // TODO: implement getTpoRatedMovie
+    throw UnimplementedError();
   }
 }
